d35aeb7ce02ce9446e790d3d85900c7a80e1eee4
rodrigoyr <ryeperub@myuax.com>
2023-12-05 10:37:01 +0100

    error coode commit

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <map>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+    Environment() {
+    }
+
+    void addSymbol(const std::string& name, int value) {
+        symbolTable[name] = value;
+    }
+
+    int getSymbolValue(const std::string& name) const {
+        auto it = symbolTable.find(name);
+
+        if (it != symbolTable.end()) {
+            return it->second;
+        } else {
+
+            std::cerr << "Error: El símbolo '" << name << "' no está definido." << std::endl;
+            return 0;
