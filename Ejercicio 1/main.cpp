ca4046ba3dbe64c476441db77083acc4f6dfe3e5
rodrigoyr <ryeperub@myuax.com>
2023-12-05 10:38:39 +0100

    fixed error code commit

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -15,11 +15,27 @@ public:
     }
 
     int getSymbolValue(const std::string& name) const {
+        // Verifica si el símbolo está en la tabla
         auto it = symbolTable.find(name);
 
         if (it != symbolTable.end()) {
             return it->second;
         } else {
-
             std::cerr << "Error: El símbolo '" << name << "' no está definido." << std::endl;
-            return 0;
+            return 0; // Valor predeterminado en caso de error
+        }
+    }
+};
+
+int main() {
+    Environment env;
+
+    env.addSymbol("x", 10);
+    env.addSymbol("y", 20);
+
+    std::cout << "Valor de x: " << env.getSymbolValue("x") << std::endl; // Debería imprimir 10
+    std::cout << "Valor de y: " << env.getSymbolValue("y") << std::endl; // Debería imprimir 20
+    std::cout << "Valor de z: " << env.getSymbolValue("z") << std::endl; // Debería imprimir un mensaje de error y 0 por defecto
+
+    return 0;
+}
