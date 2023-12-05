c9507db943c2791182dc3e331ad7cf15837e060e
rodrigoyr <ryeperub@myuax.com>
2023-12-05 11:29:07 +0100

    adding game code commit

--- Ejercicio final/main.cpp
+++ Ejercicio final/main.cpp
@@ -7,17 +7,14 @@ public:
 
     using SymbolTable = std::map<std::string, std::variant<int, double, std::string>>;
 
-
     void insert(const std::string& name, const std::variant<int, double, std::string>& value) {
 
         auto it = symbols.find(name);
         if (it != symbols.end()) {
-
             if (it->second != value) {
                 throw std::runtime_error("Error: El símbolo ya existe con un valor diferente.");
             }
         } else {
-
             symbols[name] = value;
         }
     }
@@ -30,15 +27,6 @@ public:
             throw std::runtime_error("Error: El símbolo no existe en el entorno.");
         }
     }
-
-    void remove(const std::string& name) {
-        symbols.erase(name);
-    }
-
-    bool exists(const std::string& name) const {
-        return symbols.find(name) != symbols.end();
-    }
-
 
     void printSymbols() const {
         std::cout << "Tabla de Símbolos:\n";
@@ -55,45 +43,45 @@ public:
         insert("enemyHealth", 100);
         insert("playerCoins", 50);
         insert("gameDifficulty", "Normal");
+        insert("playerWins", 0);
+    }
+
+    void incrementWins() {
+        auto wins = std::get<int>(lookup("playerWins"));
+        insert("playerWins", wins + 1);
     }
 
 private:
     SymbolTable symbols;
 };
 
+void encounterEnemy(Environment& env) {
+    std::cout << "\n¡Te encuentras con un enemigo!\n";
+    int enemyHealth = std::get<int>(env.lookup("enemyHealth"));
 
-void gameExample() {
+    std::cout << "¡Enemigo derrotado! La salud restante del enemigo es: " << enemyHealth << "\n";
+
+    env.insert("enemyHealth", enemyHealth);
+
+    env.incrementWins();
+}
+
+int main() {
     Environment env;
+
 
     env.insert("playerName", "John");
     env.insert("playerLevel", 5);
 
-    if (env.exists("playerName")) {
-        std::cout << "playerName existe en el entorno.\n";
-    }
-
-    try {
-        auto playerName = std::get<std::string>(env.lookup("playerName"));
-        std::cout << "Valor de playerName: " << playerName << "\n";
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << "\n";
-    }
-
-    try {
-        env.insert("playerLevel", 10);
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << "\n";
-    }
-
-    env.printSymbols();
-
     env.customizeGameEnvironment();
 
     env.printSymbols();
-}
 
-int main() {
-    gameExample();
+    encounterEnemy(env);
+
+    env.printSymbols();
+
+    std::cout << "\n¡Enhorabuena, has ganado!\n";
 
     return 0;
 }
