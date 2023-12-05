17b23a8d6f52ff6031ce7c3b47b4a1455589ff67
rodrigoyr <ryeperub@myuax.com>
2023-12-05 10:49:29 +0100

    first code commit

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+    Environment() {
+    }
+
+    void insertSymbol(const std::string& name, int value) {
+        auto result = symbolTable.insert({name, value});
+
+        if (result.second) {
+            std::cout << "Símbolo '" << name << "' insertado correctamente." << std::endl;
+        } else {
+            std::cerr << "Advertencia: El símbolo '" << name << "' ya existe en el entorno. No se ha realizado la inserción." << std::endl;
+        }
+    }
+
+    int lookupSymbol(const std::string& name) const {
+        auto it = symbolTable.find(name);
+
+        if (it != symbolTable.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Advertencia: El símbolo '" << name << "' no está definido en el entorno. Se devuelve el valor predeterminado 0." << std::endl;
+            return 0; // Valor predeterminado si el símbolo no está definido
+        }
+    }
+};
+
+int main() {
+    Environment env;
+
+    env.insertSymbol("x", 10);
+    env.insertSymbol("y", 20);
+    env.insertSymbol("z", 30);
+
+    std::cout << "Valor de x: " << env.lookupSymbol("x") << std::endl;
+    std::cout << "Valor de y: " << env.lookupSymbol("y") << std::endl;
+    std::cout << "Valor de z: " << env.lookupSymbol("z") << std::endl;
+
+    std::cout << "Valor de w: " << env.lookupSymbol("w") << std::endl; // Debería imprimir un mensaje de advertencia y 0 por defecto
+
+    return 0;
+}
